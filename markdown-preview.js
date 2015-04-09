7f9d37d6480b732442a2629abb287c99f4540f1c
Natalia Bryzhatenko & Stephanie Lo <pair+notika314.steppinlo@devbootcamp.com>
2015-04-09 17:25:10 -0400

    live streaming of markdown preview up and running

--- markdown-preview.js
+++ markdown-preview.js
@@ -1,18 +1,21 @@
 "use strict";
 
+// class markdownPreview
 var markdownPreviewProto = Object.create(HTMLElement.prototype);
 
 markdownPreviewProto.attachedCallback = function(){
   this.h1 = document.getElementById('h1');
   this.convert = this.convert.bind(this);
   this.result = document.getElementById('result');
+  var convertedText = this;
   this.convert();
+  document.addEventListener("input", function(){
+    convertedText.convert();
+  });
 }
 
 markdownPreviewProto.convert = function(){
-  console.log("HELLOOOO WE ARE HERE! HAPPY PANDA.");
-  // this.result.innerHTML = this.h1.value;
-  this.result.innerHTML = marked(this.h1.value)
+  this.result.innerHTML = marked(this.h1.value);
 };
 
 document.registerElement('markdown-preview', {
