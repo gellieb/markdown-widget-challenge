9bcee2cdb64febde5e986b245d7caadbaa896d21
Eveanandi Butler & Hoa Nguyen <pair+ebutler90.hoathenguyen85@devbootcamp.com>
2015-04-09 18:40:23 -0400

    Changed event listener to proper jquery function, and create div to update text to markdown

--- markdown.js
+++ markdown.js
@@ -1,13 +1,22 @@
+"use strict";
+
 var markdownProto = Object.create(HTMLElement.prototype);
 
 markdownProto.attachedCallback = function(){
   var formId = this.getAttribute("for");
+  this.convert = this.convert.bind(this);
   this.convert(formId);
 };
 
 markdownProto.convert = function(formId){
-
-
+  //console.log($("#" + formId));
+  $("#" + formId).on("change", function() {
+    console.log(this);
+    //this.innerHTML = marked($("#" + formId).val());
+    $("#update").html(marked($(this).val()));
+  });
+  //this.innerHTML = $("#" + formId).val();
+//  setTimeout(this.convert(formId), 2);
 };
 
 
