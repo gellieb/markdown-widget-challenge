e4bb97df89095f5c9ce24ed6a2134c8d4e113b60
Ryan Blecher & Awesome Ninja Fighter <pair+notryanb.samguergen@devbootcamp.com>
2015-04-09 18:25:25 -0400

    Complete markup and update

--- markup.js
+++ markup.js
@@ -1,4 +1,8 @@
+"use strict";
+
 $( document ).ready(function() {
+  // var x = $('#text-to-convert').val();
+  // console.log(x);
 /*
 <generic tag that no one ever uses by itself>
 <html> = standard html tag
@@ -12,15 +16,22 @@ Step 1. We have to Construct a Prototype that comes from HTMLElement
 /*  Line 6 creates a new object of HTMLElement, a contructor function,
     with it's prototype methods */
 // $('#text-to-convert').val()
-$('#converted-text').append($('#text-to-convert').val());
+
 
 var newHTMLTag = Object.create(HTMLElement.prototype);
 
 /* This will give the tag a specific action and make it special*/
 newHTMLTag.createdCallback = function() {
   this.current = this.lastElementChild.value;
+  this.changeText = this.changeText.bind(this);
+  this.changeText();
 };
 
+newHTMLTag.changeText = function() {
+
+  $('#converted-text').html(marked($('#text-to-convert').val()));
+  setTimeout(this.changeText, this.delay * 1 || 100);
+};
 
 var MyElement = document.registerElement('ryan-sam-tag', {prototype: newHTMLTag});
 
